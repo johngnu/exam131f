7fffce6a368093c6fc7f7a4652f8b39ac831edf3
John Castillo <john.gnu@gmail.com>
2022-12-02 13:41:34 -0400

    a1

--- arboles/src/arboles/Main.java
+++ arboles/src/arboles/Main.java
@@ -10,12 +10,10 @@ public class Main {
     public static void main(String[] args) {
         // Colas viviendas
         CSimpleV c1 = new CSimpleV();
-        c1.adicionar(new Vivienda(1, "si", "panel solar", "gas de garrafa", "si", "televisor"));
-        c1.adicionar(new Vivienda(2, "si", "motor propio", "leña", "si", "computadora"));
-        c1.adicionar(new Vivienda(1, "si", "panel solar", "gas de garrafa", "si", "televisor"));
-        c1.adicionar(new Vivienda(2, "si", "motor propio", "leña", "si", "computadora"));
-        c1.adicionar(new Vivienda(1, "si", "panel solar", "gas de garrafa", "si", "televisor"));
-        c1.adicionar(new Vivienda(2, "si", "motor propio", "leña", "si", "computadora"));
+        c1.adicionar(new Vivienda(1, "Alcantarillado", "panel solar", "gas de garrafa", "Contenedor", "televisor"));
+        c1.adicionar(new Vivienda(2, "Camara Septica", "motor propio", "Gas domiciliario", "Recoleccion", "computadora"));
+        c1.adicionar(new Vivienda(3, "Alcantarillado", "Empresa electrica", "gas de garrafa", "contenedor", "radio"));
+        c1.adicionar(new Vivienda(4, "Alcantarillado", "motor propio", "Leña", "Terreno baldio", "radio"));
         
         c1.mostrar();
         
@@ -37,13 +35,26 @@ public class Main {
         m1.setM(new Municipio("La Paz", "Murillo", cr1, zu1));
         
         NodoM m2 = new NodoM();
-        m2.setM(new Municipio("La Paz", "Larecaja", cr1, zu1));
+        m2.setM(new Municipio("La Paz", "Batallas", cr1, zu1));
         
         NodoM m3 = new NodoM();
-        m3.setM(new Municipio("La Paz", "Los Andes", cr1, zu1));
+        m3.setM(new Municipio("La Paz", "Viacha", cr1, zu1));
         
+        NodoM m4 = new NodoM();
+        m4.setM(new Municipio("La Paz", "Caranavi", cr1, zu1));
+        
+        NodoM m5 = new NodoM();
+        m5.setM(new Municipio("La Paz", "Achacachi", cr1, zu1));
+        
+        NodoM m6 = new NodoM();
+        m6.setM(new Municipio("La Paz", "Chulumani", cr1, zu1));
+        
+        // hojas
         m1.setDer(m2);
-        m2.setIzq(m3);
+        m1.setIzq(m4);        
+        m2.setIzq(m3);        
+        m4.setIzq(m5);
+        m4.setDer(m6);
         
         // ARBOL
         ArbolM a = new ArbolM();
